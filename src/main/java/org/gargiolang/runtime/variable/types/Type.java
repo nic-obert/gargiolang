55298e67ea8e3bf77f560cef03d538f69daf5f5c
SearchForMe <42468168+SearchForMe@users.noreply.github.com>
2020-11-24 11:31:24 +0100

    small changes.

--- src/main/java/org/gargiolang/runtime/variable/types/Type.java
+++ src/main/java/org/gargiolang/runtime/variable/types/Type.java
@@ -5,31 +5,9 @@ import org.gargiolang.lang.Token;
 
 abstract public class Type {
 
-    public static Token add(Object a, Token b) {
-        return null;
-    }
-
-    public static Token subtract(Object a, Token b) {
-        return null;
-    }
-
-    public static Token multiply(Object a, Token b) {
-        return null;
-    }
-
-    public static Token divide(Object a, Token b) {
-        return null;
-    }
-
-    public static Token power(Object a, Token b) {
-        return null;
-    }
-    
-    public static Token increment(Object a, Token b) {
-        return null;
-    }
-
-    public static Token decrement(Object a, Token b) {
-        return null;
-    }
+    /*
+    It doesn't make sense to have static methods in an abstract class, as, because of oop, you cannot override them, nor
+    can you force a subclass to implement it. This class should be used either for default methods, or abstract methods
+    to be overridden, but not static methods.
+     */
 }
